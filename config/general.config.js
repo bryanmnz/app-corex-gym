a3948a2174a6ac4382578eee0b47fe8a65bdda0b
Bryan Munoz <bryan.munoz.ext@claro.com.co>
2020-05-22 12:19:01 -0500

    actualización db

--- config/general.config.js
+++ config/general.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     ENVIRONMENT: process.env.NODE_ENV || 'dev',
-    MONGODB: process.env.MONGODB || 'mongodb+srv://arkix:Colombia20151@cluster0-bovde.mongodb.net/test?retryWrites=true&w=majority',
+    MONGODB: process.env.MONGODB || 'mongodb+srv://arkix:Colombia20151@cluster0-bovde.mongodb.net/appgym?retryWrites=true&w=majority',
     ///mongodb+srv://arkix:8xpveP2DHJ0Laom8@cluster0-bovde.mongodb.net/test?retryWrites=true&w=majority
     GENERIC_RESPONSE:{code:0,message:'success', body:null}
 }
